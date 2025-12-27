3b7523d8c6312f2e512b20c45808f58652ac2484
fischervorchdorf <fischervorchdorf@gmx.at>
2025-12-27 14:01:44 +0100

    v2.36: events.html komplett neu erstellt (fix corruption)

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Bump cache to force clients to fetch the latest (favorites-removed) build
-const CACHE_NAME = 'vorchdorf-app-v100';
+const CACHE_NAME = 'vorchdorf-app-v101';
 const urlsToCache = [
   './index.html',
   './manifest.json'
