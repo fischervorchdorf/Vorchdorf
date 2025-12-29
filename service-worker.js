0408999558a416e8285d566f07c9c19a8a2e16a5
fischervorchdorf <fischervorchdorf@gmx.at>
2025-12-29 07:11:41 +0100

    Update: Aktuelle index.html und service-worker.js aus Vorchdorf App/

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 // Bump cache to force clients to fetch the latest (favorites-removed) build
-const CACHE_NAME = 'vorchdorf-app-v103';
+const CACHE_NAME = 'vorchdorf-app-v104';
 const urlsToCache = [
   './index.html',
   './manifest.json'
