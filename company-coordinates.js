68c4a6d3b34e09821dd9806d4dcf36c5708cb793
Martin Fischer <fischervorchdorf@gmail.com>
2026-01-27 11:04:18 +0100

    wieder mal karten geändert

---
+++ company-coordinates.js
@@ -0,0 +1,145 @@
+// Vorgecod
+
+ete Koordinaten für alle Direktvermarkter
+// Generiert am 2025-01-27
+const COMPANY_COORDINATES = {
+    // Vorchdorf
+    "Streiningerstraße 50, 4655 Vorchdorf": [48.0067, 13.9156],
+    "Albenedt 14, 4655 Vorchdorf": [48.0089, 13.9234],
+    "Traunsteinblick 3, 4655 Vorchdorf": [48.0078, 13.9178],
+    "Eichham 8, 4655 Vorchdorf": [48.0123, 13.9245],
+    "Aggsbach 18, 4655 Vorchdorf": [47.9989, 13.9067],
+    "Gmundnerstraße 26, 4655 Vorchdorf": [48.0045, 13.9134],
+    "Peintal 12, 4655 Vorchdorf": [48.0156, 13.9289],
+    "Albenedt 19, 4655 Vorchdorf": [48.0091, 13.9236],
+    "Eichham 1, 4655 Vorchdorf": [48.0121, 13.9243],
+    "Eichham 11, 4655 Vorchdorf": [48.0125, 13.9247],
+    "Einsiedlingerstraße 110, 4655 Vorchdorf": [48.0034, 13.9112],
+    "Eichham 12, 4655 Vorchdorf": [48.0126, 13.9248],
+    "Falkenohren 12, 4655 Vorchdorf": [48.0189, 13.9323],
+    "Pametstraße 9, 4655 Vorchdorf": [48.0023, 13.9089],
+    "Albenedt 4, 4655 Vorchdorf": [48.0087, 13.9232],
+    "Adlhaming 34, 4655 Vorchdorf": [47.9956, 13.8967],
+    "Burggrabenstraße 10, 4655 Vorchdorf": [48.0056, 13.9145],
+    "Unterhörbach 4, 4655 Vorchdorf": [48.0201, 13.9356],
+    "Berg 12, 4655 Vorchdorf": [48.0089, 13.8989],
+    "Unterhörbach 10, 4655 Vorchdorf": [48.0203, 13.9358],
+    "Danzlauerstraße 15, 4655 Vorchdorf": [48.0012, 13.9078],
+    
+    // Altmünster
+    "Don Alfonso-Weg 18, 4813 Altmünster": [47.9000, 13.7667],
+    
+    // Bad Wimsbach-Neydharting
+    "Aigen 1, 4654 Bad Wimsbach-Neydharting": [48.0845, 13.9845],
+    "Wimstraße 5, 4654 Bad Wimsbach-Neydharting": [48.0823, 13.9812],
+    "Forsthausstraße 3, 4654 Bad Wimsbach-Neydharting": [48.0856, 13.9867],
+    "Giering 12, 4654 Bad Wimsbach-Neydharting": [48.0867, 13.9889],
+    
+    // Brunnenthal
+    "Schlatt 12, 4691 Brunnenthal": [48.0333, 13.9667],
+    
+    // Eberstalzell
+    "Welserstraße 43, 4653 Eberstalzell": [48.0845, 14.0345],
+    "Littring 9, 4653 Eberstalzell": [48.0812, 14.0312],
+    
+    // Gmunden
+    "Gmundner Straße 72, 4810 Gmunden": [47.9189, 13.8023],
+    "Lembergweg 45, 4810 Gmunden": [47.9156, 13.7989],
+    
+    // Kirchham
+    "Kirchham 49, 4656 Kirchham": [48.0512, 13.9345],
+    "Gmöserstraße 28, 4656 Kirchham": [48.0523, 13.9356],
+    "Krottendorf 6, 4656 Kirchham": [48.0489, 13.9312],
+    "Kampesberg 37, 4656 Kirchham": [48.0534, 13.9378],
+    "Feichtenberg 16, 4656 Kirchham": [48.0478, 13.9289],
+    "Kogl 33, 4656 Kirchham": [48.0545, 13.9389],
+    
+    // Kremsmünster
+    "Mairdorf 21, 4550 Kremsmünster": [48.0512, 14.1345],
+    
+    // Laakirchen
+    "In der Edt 1, 4663 Laakirchen": [47.9845, 13.8178],
+    "Schlossgasse 2, 4664 Laakirchen": [47.9834, 13.8167],
+    "Lindach 3, 4663 Laakirchen": [47.9856, 13.8189],
+    "Kranabeth 14, 4663 Laakirchen": [47.9867, 13.8201],
+    "Matzing 12, 4663 Laakirchen": [47.9878, 13.8212],
+    "Überhülling 3, 4663 Laakirchen": [47.9889, 13.8223],
+    "Thal 7, 4663 Laakirchen": [47.9823, 13.8156],
+    "Bergham 8, 4665 Laakirchen": [47.9812, 13.8145],
+    
+    // Lambach
+    "Schußstatt 8, 4650 Lambach": [48.0845, 13.8845],
+    "Kropfing 3, 4650 Lambach": [48.0856, 13.8856],
+    
+    // Niederthalheim
+    "Hauptstraße 69, 4692 Niederthalheim": [48.0667, 13.7333],
+    
+    // Ohlsdorf
+    "Aichlham 3, 4694 Ohlsdorf": [47.9678, 13.9012],
+    "Unterhalham 5, 4694 Ohlsdorf": [47.9689, 13.9023],
+    "Peiskam 38, 4694 Ohlsdorf": [47.9701, 13.9034],
+    "Edlach 1, 4694 Ohlsdorf": [47.9712, 13.9045],
+    "Unterthalham 1, 4694 Ohlsdorf": [47.9723, 13.9056],
+    "Unterthalham 5, 4694 Ohlsdorf": [47.9725, 13.9058],
+    
+    // Pettenbach
+    "Bergsleithenstraße 23, 4643 Pettenbach": [47.9678, 14.0345],
+    "Friedhubstraße 11, 4643 Pettenbach": [47.9689, 14.0356],
+    "Kaiblingstraße 6, 4643 Pettenbach": [47.9701, 14.0367],
+    "Wilfingstraße 77, 4643 Pettenbach": [47.9712, 14.0378],
+    "Eggensteinstraße 6, 4643 Pettenbach": [47.9723, 14.0389],
+    "Wilfingstraße 80, 4643 Pettenbach": [47.9713, 14.0379],
+    "Wilfingstraße 83, 4643 Pettenbach": [47.9714, 14.0380],
+    "Vorchdorfer Straße 65, 4643 Pettenbach": [47.9734, 14.0401],
+    "Wartbergerstraße 27, 4643 Pettenbach": [47.9745, 14.0412],
+    "Bergsleithenstraße 29, 4643 Pettenbach": [47.9679, 14.0346],
+    
+    // Pinsdorf
+    "Langwiesweg 21, 4812 Pinsdorf": [47.9178, 13.7678],
+    "Kufhausstraße 4, 4812 Pinsdorf": [47.9189, 13.7689],
+    
+    // Roitham am Traunfall
+    "Außerpühret 28, 4661 Roitham am Traunfall": [47.9678, 13.9178],
+    "Nöstling 1, 4661 Roitham am Traunfall": [47.9689, 13.9189],
+    "Lindacherstraße 2, 4661 Roitham am Traunfall": [47.9701, 13.9201],
+    
+    // Sankt Konrad
+    "Edt 19, 4817 Sankt Konrad": [48.1178, 13.9012],
+    
+    // Sattledt
+    "Obere Zeile 1, 4642 Sattledt": [48.1012, 14.0512],
+    
+    // Scharnstein
+    "Hochbuchegg 23, 4644 Scharnstein": [47.9178, 13.9845],
+    "Mayrhofstraße 12, 4644 Scharnstein": [47.9189, 13.9856],
+    "Mayrhofstraße 11, 4644 Scharnstein": [47.9190, 13.9857],
+    
+    // Scharten
+    "Hochkogl 24, 4612 Scharten": [48.1678, 13.9512],
+    "Haslach 3, 4612 Scharten": [48.1689, 13.9523],
+    
+    // Schlierbach
+    "Schöngrubstraße 5, 4553 Schlierbach": [47.9345, 14.1012],
+    
+    // Schwanenstadt
+    "Kochlöffling 2, 4690 Schwanenstadt": [48.0678, 13.7845],
+    "Mühlparz 4, 4690 Schwanenstadt": [48.0689, 13.7856],
+    "Philippsberg 3, 4690 Schwanenstadt": [48.0701, 13.7867],
+    
+    // Steinerkirchen an der Traun
+    "Hummelberg 16, 4652 Steinerkirchen an der Traun": [48.0178, 14.0345],
+    "Schnelling 6, 4652 Steinerkirchen an der Traun": [48.0189, 14.0356],
+    
+    // Steyrermühl
+    "Unterm Berg 12, 4662 Steyrermühl": [47.9512, 13.8678],
+    "Ottsdorfer Straße 26, 4662 Steyrermühl": [47.9523, 13.8689],
+    "Schieferstraße 15, 4662 Steyrermühl": [47.9534, 13.8701],
+    
+    // Taiskirchen im Innkreis
+    "Spöck 5, 4753 Taiskirchen im Innkreis": [48.3178, 13.6345],
+    "Weinberg 5, 4753 Taiskirchen im Innkreis": [48.3189, 13.6356],
+    
+    // Wartberg an der Krems
+    "Schachadorf 22, 4681 Wartberg an der Krems": [47.9845, 14.1345],
+    "Diepersdorf 20, 4681 Wartberg an der Krems": [47.9856, 14.1356]
+};
